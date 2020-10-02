58152dd3b25c657a9e8be8dc93e7419ba0745bcf
DoomyTheFroomy <m.rudolph@email.de>
2020-10-02 10:30:50 +0200

    Created scraper to fetch services information for berlin

--- scraper.js
+++ scraper.js
@@ -1,59 +1,137 @@
 // This is a template for a Node.js scraper on morph.io (https://morph.io)
 
-var cheerio = require("cheerio");
-var request = require("request");
-var sqlite3 = require("sqlite3").verbose();
+const cheerio = require('cheerio')
+const request = require('request')
+const sqlite3 = require('sqlite3').verbose()
+const URL = require('url').URL
+const db = new sqlite3.Database('data.sqlite')
 
-function initDatabase(callback) {
-	// Set up sqlite database.
-	var db = new sqlite3.Database("data.sqlite");
-	db.serialize(function() {
-		db.run("CREATE TABLE IF NOT EXISTS data (name TEXT)");
-		callback(db);
-	});
+const baseUrl = 'https://service.berlin.de'
+
+const services = {}
+
+function initDatabase (callback) {
+  // Set up sqlite database.
+  db.serialize(function () {
+    db.run('DROP TABLE "data";', function () {
+      db.run('CREATE TABLE IF NOT EXISTS data (id TEXT, title TEXT, description TEXT, Voraussetzungen TEXT, "Erforderliche Unterlagen" TEXT, Formulare TEXT, "Gebühren" Text, Rechtsgrundlagen TEXT, "Weiterführende Informationen" TEXT, "Zuständige Behörden" TEXT)', callback(db))
+    })
+  })
 }
 
-function updateRow(db, value) {
-	// Insert some data.
-	var statement = db.prepare("INSERT INTO data VALUES (?)");
-	statement.run(value);
-	statement.finalize();
+function updateRow (value) {
+  console.log('value', value)
+  // Insert some data.
+  var insert = 'INSERT INTO data (id,title,description,Voraussetzungen,Rechtsgrundlagen,"Erforderliche Unterlagen",Formulare,"Gebühren","Weiterführende Informationen","Zuständige Behörden") VALUES ($id,$title,$description,$Voraussetzungen,$Rechtsgrundlagen,$ErforderlicheUnterlagen,$Formulare,$Gebuehren,$WeiterfuehrendeInformationen,$ZustaendigeBehoerden);'
+  db.run(insert, {
+    $id: value['id'],
+    $title: value['title'],
+    $description: value['description'],
+    $Voraussetzungen: JSON.stringify(value['Voraussetzungen']),
+    $Rechtsgrundlagen: JSON.stringify(value['Rechtsgrundlagen']),
+    $ErforderlicheUnterlagen: JSON.stringify(value['Erforderliche Unterlagen']),
+    $Formulare: JSON.stringify(value['Formulare']),
+    $Gebuehren: JSON.stringify(value['Gebühren']),
+    $WeiterfuehrendeInformationen: JSON.stringify(value['Weiterführende Informationen']),
+    $ZustaendigeBehoerden: JSON.stringify(value['Zuständige Behörden'])
+  })
+  // const statement = db.run('INSERT INTO data VALUES ', value)
+  // statement.run(value)
+  // statement.finalize()
 }
 
-function readRows(db) {
-	// Read some data.
-	db.each("SELECT rowid AS id, name FROM data", function(err, row) {
-		console.log(row.id + ": " + row.name);
-	});
+function readRows (db) {
+  // Read some data.
+  db.each('SELECT rowid AS id, name FROM data', function (err, row) {
+    console.log(row.id + ': ' + row.name)
+  })
 }
 
-function fetchPage(url, callback) {
-	// Use request to read in pages.
-	request(url, function (error, response, body) {
-		if (error) {
-			console.log("Error requesting page: " + error);
-			return;
-		}
+function fetchPage (url, callback) {
+  // Use request to read in pages.
+  request(url, function (error, response, body) {
+    if (error) {
+      console.log('Error requesting page: ' + error)
+      return
+    }
 
-		callback(body);
-	});
+    callback(body, url)
+  })
 }
 
-function run(db) {
-	// Use request to read in pages.
-	fetchPage("https://morph.io", function (body) {
-		// Use cheerio to find things in the page with css selectors.
-		var $ = cheerio.load(body);
+function run (db) {
+  // Use request to read in pages.
+  fetchPage(baseUrl + '/dienstleistungen', function (body) {
+    // Use cheerio to find things in the page with css selectors.
+    const $ = cheerio.load(body)
 
-		var elements = $("div.media-body span.p-name").each(function () {
-			var value = $(this).text().trim();
-			updateRow(db, value);
-		});
+    $('ul.list li.topic-dls a').each(function (i) {
+      // if (i > 10) return false
+      const href = $(this).attr('href')
+      console.log(href)
+      setTimeout(() => {
+        fetchPage(baseUrl + href, getServiceData)
+      }, i * 300)
 
-		readRows(db);
+      // updateRow(db, value)
+    })
 
-		db.close();
-	});
+    // readRows(db)
+
+    // db.close()
+  })
 }
 
-initDatabase(run);
+function getServiceData (body, url) {
+  const Url = new URL(url)
+  // console.log(Url.pathname)
+  const id = Url.pathname.split('/')[2]
+  console.log(id)
+  // console.log(url)
+  const $ = cheerio.load(body)
+  const title = $('h1.title').text()
+  // const serviceBody = $('div.body.dienstleistung')
+  console.log(title)
+  // console.log(serviceBody)
+  services[id] = {
+    id: id,
+    title: title
+  }
+
+  $('div.body.dienstleistung div.block').each(function (i, elem) {
+    if (i === 0) {
+      console.log(i, $(this).text().trim())
+      services[id]['description'] = $(this).text().trim()
+    }
+    const topic = $(this).find('h2').text()
+    if (topic.length > 0) {
+    // console.log(topic)
+      services[id][topic] = []
+      let title
+      let description
+      let href
+      if (topic === 'Zuständige Behörden') {
+        $(this).find('div.behoerdenitem').each(function (index, el) {
+          title = $(this).find('strong').text().trim()
+          description = $(this).find('p.inner').text().trim()
+          href = $(this).find('a').attr('href')
+        })
+      } else {
+        // if ($(this).find('h2').text() === 'Voraussetzungen') {
+        $(this).find('li').each(function (index, el) {
+          title = ($(this).find('strong').length > 0 ? $(this).find('strong').text().trim() : $(this).text().trim())
+          description = $(this).find('div.description').text().trim()
+          href = $(this).find('a').attr('href')
+          console.log(topic, title, description, $(this).text().trim())
+        })
+      }
+      services[id][topic].push({ title: title, description: description, href: href })
+    }
+    // }
+  })
+
+  console.log(services[id])
+  updateRow(services[id])
+}
+
+initDatabase(run)
